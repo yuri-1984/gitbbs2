73f002fe22e40ad9c9dcfdf2bfec6dbe52ce6707
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2019-11-22 10:59:25 +0900

    プロジェクト新規作成

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Car {
+	private Integer speed;
+	private String bodyColor;
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+
+}
